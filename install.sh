25c45b07d6bcec6da577e67dcb6f46ae53888b19
kevintee23 <31426495+kevintee23@users.noreply.github.com>
2019-04-23 17:16:47 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@
 # by de_man
 # 23/4/2019
 
+cd
+
 echo '[+] Installing and updating core dependencies...'
 apt-get update && apt-get -y upgrade
 
-echo '[+] Installing npm, pip and gunicorn...'
-sudo apt-get install npm python-pip gunicorn
+echo '[+] Installing npm...'
+sudo apt-get install npm
+
+echo '[+] Installing pip and gunicorn...'
+sudo apt-get install python-pip gunicorn
 
 echo '[+] Downloading the latest and stable version of nodejs...'
 sudo npm cache clean -f
@@ -24,6 +29,6 @@ echo '[+] Installing flask...'
 sudo pip install flask
 
 echo '[+] Cloning gunicorn service file to the appropriate folder'
-sudo cp ~/speedtest/gunicorn.service /etc/systemd/system/
+sudo cp /home/pi/speedtest/gunicorn.service /etc/systemd/system/
 
 echo 'May the force be with you...'
